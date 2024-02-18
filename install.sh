0631cce761747be13ff957a44a941da894e2330e
temakrivolapov <138175570+medowic@users.noreply.github.com>
2024-02-19 00:13:54 +0300

    v1.0: initial

--- install.sh
+++ install.sh
@@ -1,74 +1,46 @@
 #!/bin/bash
 
-## ServerInColour Version 0.1-beta
+## ServerInColour v1.0
 ## Make your home page colorful and readable!
-##
-## Installation file
 ##
 ## Repository: https://github.com/medowic/serverincolour
 
+## Installation file
+
 RED='\033[31m'
 ORANGE='\033[33m'
-GREEN='\033[92m'
 NC='\033[0m'
 
 WORK_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-tput reset
-
-echo "ServerInColour // Version 0.1-beta // Install"
-echo ""
-
-## Check root
-
 if [ "${EUID}" -ne 0 ]; then
-
-    echo -e -n "${RED}You need to run this script as root.${NC} Are you root?";
-    echo "";
-
+    echo -e "${RED}[ ERROR ]${NC}: You need to run this script as root";
     exit 1;
-
 fi
 
-## Check file exist
+# shellcheck disable=SC1091
+source /etc/os-release
+OS_ID="${ID}"
 
-if ! [ -f "${WORK_DIR}/sic.sh" ]; then
-
-    echo -e "${RED}Cannot to find 'sic.sh' (general) file.${NC}";
-    echo "Re-download git-repository and try again.";
-    echo "";
-    
-    exit 1;
-
+if [ "${OS_ID}" == "ubuntu" ] || [ "${OS_ID}" == "debian" ] || [ "${OS_ID}" == "raspbian" ]; then
+    mkdir /root/.serverincolour
+    cp "${WORK_DIR}"/update-service.sh /root/.serverincolour
+    cp "${WORK_DIR}"/sic-update.service /etc/systemd/system
+    systemctl daemon-reload >/dev/null 2>&1
+    systemctl enable sic-update >/dev/null 2>&1
+else
+    echo -e "${ORANGE}[ WARN ]${NC}: update information couldn't be displayed on your system"
 fi
-
-## Start of code
+unset OS_ID RED ORANGE NC
 
 cp "${WORK_DIR}"/sic.sh /etc/profile.d
-
-if [ "${?}" == "1" ]; then
-
-    echo -e "${RED}Something went wrong...${NC}";
-    echo "";
-    echo "We couldn't copy general file to /etc/profile.d";
-    echo "Sorry =(";
-    echo "";
-
-    exit 1;
-
-fi
-
 chmod u+x /etc/profile.d/sic.sh
 chmod 777 /etc/profile.d/sic.sh
 
-echo -e "${GREEN}Done!${NC} You successful installed ServerInColour. Thank you for install! =)"
 echo ""
-echo "Now when you log in, you will see the ServerInColour home screen."
-echo -e "And remember - ${ORANGE}this is beta-version${NC}. If you see a bug or just have a suggestion, tell us about it at GitHub discussion!"
+echo "ServerInColour was installed"
+echo "Now when you log in, you will see the ServerInColour home screen"
 echo ""
-echo "                       Repository: https://github.com/medowic/serverincolour"   
+echo "For uninstall, use ./uninstall.sh in the same directory where located ./install.sh"
 echo ""
-echo "For uninstall, check https://github.com/medowic/serverincolour#Uninstall"
-echo ""
-
 exit 0
