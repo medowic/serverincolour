f300f44f145be1e5d5095f77a6cbb41d8b2cf3d0
temakrivolapov <138175570+medowic@users.noreply.github.com>
2024-02-19 00:14:24 +0300

    v1.0: initial

--- uninstall.sh
+++ uninstall.sh
@@ -1,57 +1,33 @@
 #!/bin/bash
 
-## ServerInColour Version 0.1-beta
+## ServerInColour v1.0
 ## Make your home page colorful and readable!
-##
-## Uninstallation file
 ##
 ## Repository: https://github.com/medowic/serverincolour
 
+## Uninstallation file
+
 RED='\033[31m'
-ORANGE='\033[33m'
-GREEN='\033[92m'
 NC='\033[0m'
 
-tput reset
-
-echo "ServerInColour // Version 0.1-beta // Uninstall"
-echo ""
-
-## Check root
-
 if [ "${EUID}" -ne 0 ]; then
-
-    echo -e -n "${RED}You need to run this script as root.${NC} Are you root?";
-    echo "";
-
+    echo -e "${RED}[ ERROR ]${NC}: You need to run this script as root";
     exit 1;
-
 fi
 
-## Start of code
+if [ -f /etc/systemd/system/sic-update.service ] || [ -d /root/.serverincolour ]; then
+    rm -rf /root/.serverincolour
+    rm -d /root/.serverincolour
+    rm /etc/systemd/system/sic-update.service
+    systemctl daemon-reload >/dev/null 2>&1
+fi
 
 rm /etc/profile.d/sic.sh
 
-if [ "${?}" == "1" ]; then
-
-    echo -e "${RED}Something went wrong...${NC}";
-    echo "";
-    echo "We couldn't delete general file ('sic.sh') in /etc/profile.d";
-    echo "Sorry =(";
-    echo "";
-
-    exit 1;
-
-fi
-
-echo -e "${GREEN}Done!${NC} You successful deleted ServerInColour. Thank you for using."
 echo ""
+echo "ServerInColour was deleted"
 echo "Now when you log in, you will see the standart system home screen."
 echo ""
-echo -e "${ORANGE}We would be very grateful, if you could tell us why you deleted ServerInColour.${NC}"
-echo "                Tell us about it in the discussion on GitHub"
-echo ""
-echo "            Repository: https://github.com/medowic/serverincolour"   
-echo ""
 
+unset RED NC
 exit 0
